7b9cff25703499ce8d232137c628c01a08fba844
Sriharsha Vardhan <sriharshavardhan.t@us.abb.com>
2019-01-26 03:12:38 -0800

    fix errors with TryBool

--- examples/ex01.cpp
+++ examples/ex01.cpp
@@ -5,16 +5,16 @@ using namespace std;
 using namespace abb::rws;
 int main(int argc, char* argv[]) {
 
-    auto rws_interface = new RWSInterface("192.168.132.6", 80);
+    auto rws_interface = new RWSInterface("localhost", 80);
 
     auto static_info = rws_interface->collectStaticInfo();
     cout << "Robotware Version # " << static_info.system_info.robot_ware_version << endl;
     cout << "System Name # " << static_info.system_info.system_name << endl;
 
     auto runtime_info = rws_interface->collectRuntimeInfo();
-    cout << "Mode # " << (runtime_info.auto_mode ? "Auto" : "Manual") << endl;
-    cout << "Motor # " << (runtime_info.motor_on ? "ON" : "OFF") << endl;
-    cout << "Rapid # " << (runtime_info.rapid_running ? "Running" : "Stopped") << endl;
+    cout << "Mode # " << (runtime_info.auto_mode.isTrue() ? "Auto" : "Manual") << endl;
+    cout << "Motor # " << (runtime_info.motor_on.isTrue() ? "ON" : "OFF") << endl;
+    cout << "Rapid # " << (runtime_info.rapid_running.isTrue() ? "Running" : "Stopped") << endl;
     cout << "Status # " << (runtime_info.rws_connected ? "Connected" : "Disconnected") << endl;
 
     delete rws_interface;
